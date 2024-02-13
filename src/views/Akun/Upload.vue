bafea028577a70902e1b7f588ccda4fd263d4cc6
akufarish <risfarishk@gmail.com>
2024-02-13 13:38:48 +0800

    add dropdown in add project

--- src/views/Akun/Upload.vue
+++ src/views/Akun/Upload.vue
@@ -112,24 +112,92 @@
         <div class="flex gap-36">
           <label for="tools">Tools :</label>
           <div class="flex flex-col gap-2">
-            <input
-              type="text"
-              id="tools"
-              class="py-2 w-[365px] bg-transparent bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-transparent dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Design: Figma"
-            />
-            <input
-              type="text"
-              id="tools"
-              class="py-2 w-[365px] bg-transparent bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-transparent dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Code Editor: VS Code"
-            />
-            <input
-              type="text"
-              id="tools"
-              class="py-2 w-[365px] bg-transparent bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-transparent dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Bahasa Pemrograman: Tailwind CSS, dan Vue.js"
-            />
+            <ul
+              class="flex flex-wrap w-[350px] gap-5"
+              v-if="selected.length > 0"
+            >
+              <li
+                class="bg-secondary p-1 dark:bg-primary rounded-full text-primary dark:text-secondary w-20 flex justify-start items-center"
+                v-for="tools in selected"
+                :key="tools.id"
+              >
+                <h3 @click="destroy(tools.id)" class="text-center w-full">
+                  {{ tools.name }}
+                </h3>
+              </li>
+            </ul>
+            <Combobox v-model="selected" multiple>
+              <div class="relative mt-1">
+                <div class="">
+                  <ComboboxInput
+                    class="py-2 w-[365px] bg-transparent border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-transparent dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    :displayValue="(tool) => tool.name"
+                    @change="query = $event.target.value"
+                  />
+                  <ComboboxButton
+                    class="absolute inset-y-0 right-0 flex items-center pr-2"
+                  >
+                    <ChevronUpDownIcon
+                      class="h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </ComboboxButton>
+                </div>
+                <TransitionRoot
+                  leave="transition ease-in duration-100"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                  @after-leave="query = ''"
+                >
+                  <ComboboxOptions
+                    class="py-2 w-[365px] bg-transparent border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-transparent dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  >
+                    <div
+                      v-if="filter.length === 0 && query !== ''"
+                      class="relative cursor-default select-none px-4 py-2 text-gray-700"
+                    >
+                      Nothing found.
+                    </div>
+
+                    <ComboboxOption
+                      v-for="tool in filter"
+                      as="template"
+                      :key="tool.id"
+                      :value="tool"
+                      v-slot="{ selected, active }"
+                    >
+                      <li
+                        class="relative cursor-default select-none py-2 pl-10 pr-4"
+                        :class="{
+                          'bg-primary text-secondary': active,
+                          'text-blue-400': !active,
+                        }"
+                      >
+                        <span
+                          class="block truncate"
+                          :class="{
+                            'font-medium': selected,
+                            'font-normal': !selected,
+                          }"
+                        >
+                          {{ tool.name }}
+                        </span>
+                        <span
+                          v-if="selected"
+                          class="absolute inset-y-0 left-0 flex items-center pl-3"
+                          :class="{
+                            'text-green-600': active,
+                            'text-neutral-300 font-bold': !active,
+                          }"
+                        >
+                          <CheckIcon class="h-6 w-6" aria-hidden="true" />
+                        </span>
+                      </li>
+                    </ComboboxOption>
+                  </ComboboxOptions>
+                </TransitionRoot>
+              </div>
+            </Combobox>
           </div>
         </div>
 
@@ -147,27 +215,46 @@
   </div>
 </template>
 <script setup>
-import { ref, reactive } from "vue";
+import { ref, reactive, computed } from "vue";
 import {
   PencilSquareIcon,
   ArrowLeftIcon,
   HomeIcon,
   ChevronRightIcon,
+  CheckIcon,
+  ChevronUpDownIcon,
 } from "@heroicons/vue/24/solid";
 import useProject from "../../services/project/index";
 import Navbar from "../../components/Navbar.vue";
 import NavSidebar from "../../components/NavSidebar.vue";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxButton,
+  ComboboxOptions,
+  ComboboxOption,
+  TransitionRoot,
+} from "@headlessui/vue";
 
 const Tools = [
-  { id: 1, name: "HTML", icon: "../../../src/assets/iconTools/html.svg" },
-  { id: 2, name: "Laptop", icon: "../../../src/assets/iconTools/css.svg" },
-  { id: 3, name: "a", icon: "../../../src/assets/iconTools/css.svg" },
-  { id: 4, name: "b", icon: "../../../src/assets/iconTools/css.svg" },
-  { id: 5, name: "c", icon: "../../../src/assets/iconTools/css.svg" },
-  { id: 6, name: "d", icon: "../../../src/assets/iconTools/css.svg" },
-  { id: 7, name: "e", icon: "../../../src/assets/iconTools/css.svg" },
-  { id: 8, name: "f", icon: "../../../src/assets/iconTools/css.svg" },
+  { id: 1, name: "HTML" },
+  { id: 2, name: "Laptop" },
+  { id: 3, name: "figma" },
+  { id: 4, name: "css" },
+  { id: 5, name: "php" },
+  { id: 6, name: "laravel" },
+  { id: 7, name: "javascript" },
+  { id: 8, name: "f" },
 ];
+
+const selected = ref([]);
+const query = ref("");
+
+function destroy(id) {
+  console.log(id);
+  console.log(selected.value);
+  selected.value.splice(selected.value.indexOf(id), 1);
+}
 
 const projectPicturePreview = ref(null);
 
@@ -203,6 +290,17 @@ const getImage = ($event) => {
   form.image = target.files[0];
   form.preview = URL.createObjectURL($event.target.files[0]);
 };
+
+let filter = computed(() =>
+  query.value === ""
+    ? Tools
+    : Tools.filter((tool) =>
+        tool.name
+          .toLowerCase()
+          .replace(/\s+/g, "")
+          .includes(query.value.toLowerCase().replace(/\s+/g, ""))
+      )
+);
 
 const { StoreProject } = useProject();
 
